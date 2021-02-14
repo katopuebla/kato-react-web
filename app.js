cef3d3351fb1c8f82a6150e5d82c05d492cde61e
Kato <katopuebla@gmail.com>
2021-02-14 03:55:34 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+      <button onClick={ () => this.setState({ liked: true }) }>
+      Like
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
